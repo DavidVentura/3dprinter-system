a00bba4c9a7885c6443fc2696a515b7325ffe370
david <davidventura27@gmail.com>
2019-08-11 11:05:25 +0200

    remove poweroff as its async now

--- print_file.sh
+++ print_file.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eu
-function finish {
-    echo "Exiting in 5 seconds.."
-    sleep 5
-    mosquitto_pub -h iot.labs -t "PRINTER_POWER/set" -m 0
-}
+#function finish {
+#    echo "Exiting in 5 seconds.."
+#    sleep 5
+#    mosquitto_pub -h iot.labs -t "PRINTER_POWER/set" -m 0
+#}
 
-trap finish EXIT
+#trap finish EXIT
 [ -f "$1" ] || (echo "File $1 does not exist" && exit 1)
 mosquitto_pub -h iot.labs -t "PRINTER_POWER/set" -m 1
 sleep 1
