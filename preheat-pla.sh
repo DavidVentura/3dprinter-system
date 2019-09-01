c45498550b8f406bc9fb80952317925cb4ed150f
david <davidventura27@gmail.com>
2019-09-01 11:54:17 +0200

    turn printer on and heat up extruder on preheat

--- preheat-pla.sh
+++ preheat-pla.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+mosquitto_pub -h iot.labs -t "PRINTER_POWER/set" -m 1
+sleep 1
 
 # bed async
 mosquitto_pub -h iot.labs -t 'printer/commands' -m 'raw M140 S50'
+# extruder async
+mosquitto_pub -h iot.labs -t 'printer/commands' -m 'raw M104 S180'
 
 # bed sync
 #mosquitto_pub -h iot.labs -t topic -m 'M190 S50'
